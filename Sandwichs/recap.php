722a9576140238844c4d8112dbb71765707c3118
Yasmine Gharbi <99125118+GHARBIyasmine@users.noreply.github.com>
2022-04-01 12:28:10 +0000

    Update recap.php

--- Sandwichs/recap.php
+++ Sandwichs/recap.php
@@ -20,7 +20,7 @@ $total= $price*$num;
 <head>
     <meta charset="UTF-8">
     <title>recap</title>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://gharbiyasmine.github.io/Atelier-PHP/bootstrap.css">
     <script src="https://kit.fontawesome.com/ce61b18206.js" crossorigin="anonymous"></script>
 
 </head>
